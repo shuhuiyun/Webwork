1035cce8e30de64682d51272f95deecfd8f3b255
shuhuiyun <wanan850405@gmail.com>
2023-08-31 17:04:16 +0800

    0831

--- 12_Project_20230828/javascripts/all.js
+++ 12_Project_20230828/javascripts/all.js
@@ -4,3 +4,12 @@ toggleMenuBtn.addEventListener("click", (evt) => {
   evt.preventDefault();
   body.classList.toggle("sidebar__toggled");
 });
+
+const modalByDelete = document.querySelector("#deleteModal");
+modalByDelete.addEventListener("show.bs.modal", function (event) {
+  const button = event.relatedTarget;
+  const orderId = button.dataset.bsOrderId;
+  console.log(orderId);
+  const modalText = modalByDelete.querySelector("#deleteText");
+  modalText.textContent = orderId;
+});
