91dd06a62dae0c185be004577ba4d18400fa86ec
shuhuiyun <wanan850405@gmail.com>
2023-09-05 19:08:33 +0800

    0905

--- 12_Project_20230828/javascripts/all.js
+++ 12_Project_20230828/javascripts/all.js
@@ -1,17 +1,9 @@
+//左側選單
 const toggleMenuBtn = document.querySelector("#toggle-btn");
 const body = document.querySelector("body");
 toggleMenuBtn.addEventListener("click", (evt) => {
   evt.preventDefault();
   body.classList.toggle("sidebar__toggled");
-});
-
-const modalByDelete = document.querySelector("#deleteModal");
-modalByDelete.addEventListener("show.bs.modal", function (event) {
-  const button = event.relatedTarget;
-  const orderId = button.dataset.bsOrderId;
-  console.log(orderId);
-  const modalText = modalByDelete.querySelector("#deleteText");
-  modalText.textContent = orderId;
 });
 
 //動態圖表
@@ -100,3 +92,13 @@ modalByDelete.addEventListener("show.bs.modal", function (event) {
   const pieChart = new Chart(pieCtx, config);
   const barChart = new Chart(barCtx, barConfig);
 })();
+
+//Modal
+const modalByDelete = document.querySelector("#deleteModal");
+modalByDelete.addEventListener("show.bs.modal", function (event) {
+  const button = event.relatedTarget;
+  const orderId = button.dataset.bsOrderId;
+  console.log(orderId);
+  const modalText = modalByDelete.querySelector("#deleteText");
+  modalText.textContent = orderId;
+});
